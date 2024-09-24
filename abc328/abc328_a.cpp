3662dd7bc73a10f02becf05c69dfc05d0b31dd4c
Itsuki54 <5shi.icyu@gmail.com>
2024-09-24 02:00:36 +0000

    Added my codes from AtCoder

---
+++ abc328/abc328_a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+#define inf 1ll << 60
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define pb push_back
+#define sz(x) (ll)(x).size()
+#define print(x) cout << x << endl
+const vector<int> dx = {-1, 0, 1, 0};
+const vector<int> dy = {0, 1, 0, -1};
+template <typename T>
+void chmin(T &a, T b)
+{
+    a = min(a, b);
+}
+
+template <typename T>
+void chmax(T &a, T b)
+{
+    a = max(a, b);
+}
+
+ll POW(ll x, ll n)
+{
+    ll res = 1;
+    while (n > 0)
+    {
+        if (n & 1)
+            res *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    ll N,X;
+    cin >> N >> X;
+    ll ans = 0;
+    rep(i, N)
+    {
+        ll a;
+        cin >> a;
+        if(a<=X)
+            ans += a;
+    }
+    cout << ans << endl;
+}
